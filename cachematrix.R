eb15ec6277e61291bb0bce13c54b7a179a4873eb
Li <lij464@nala.roche.com>
2025-08-07 21:56:28 -0700

    Completed assignment: implemented makeCacheMatrix and cacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,34 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL  # cache for inverse
+  
+  set <- function(y) {
+    x <<- y       # update matrix
+    inv <<- NULL  # reset cached inverse
+  }
+  
+  get <- function() x  # return the matrix
+  
+  setinverse <- function(inverse) inv <<- inverse  # cache the inverse
+  
+  getinverse <- function() inv  # return the cached inverse
+  
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinverse()
+  
+  if (!is.null(inv)) {
+    message("Getting cached inverse...")
+    return(inv)
+  }
+  
+  mat <- x$get()
+  inv <- solve(mat, ...)  # compute inverse
+  x$setinverse(inv)       # cache it
+  inv
 }
